38379a91e28aae059a2b8d03f4076e7bdeaed8d7
Monisha P C <monishapc2@gmail.com>
2026-08-19 11:42:35 +0530

    new commit

---
+++ day2/script.js
@@ -0,0 +1,32 @@
+// const college = "rit"
+// colege = "RIT"
+
+// let name="Alice"
+// let age=20;
+// let isStudent=true;
+// let salary=null;
+// let address=undefined;
+// console.log(typeof name); // string
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof salary);
+// console.log(typeof address);
+
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// let age=20;
+// console.log(age>18);
+// console.log(age<18);
+// console.log(age==20);
+// console.log(age!=20);
+
+let title=document.getElementById("title");
+let button=document.getElementById("btn");
+button.addEventListener("click",function(){
+    title.innerHTML="Button clicked";
+});
